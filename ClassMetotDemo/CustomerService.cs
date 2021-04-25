a10b32d9a1b5b6b1efc3d7299836566985e9fac7
furkan turan <furkanturan10r@hotmail.com>
2021-04-25 23:27:20 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerService
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " başarılı bir şekilde eklendi.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " başarılı bir şekilde silindi.");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " başarılı bir şekilde listeye eklendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Furkan";
+            customer.Surname = "Turan";
+            customer.Id = 1;
+            customer.Email = "furkanturan10r@hotmail.com";
+
+            CustomerService customerService = new CustomerService();
+            customerService.Add(customer);
+            customerService.Delete(customer);
+            customerService.List(customer);
+
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
